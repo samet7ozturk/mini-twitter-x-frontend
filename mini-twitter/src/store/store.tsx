81d3c1c72a2fcf026c21d5ceacf0700d291b72d8
samet7ozturk <sametozturk973@gmail.com>
2024-03-09 22:26:36 +0300

    User reducer added

--- mini-twitter/src/store/store.tsx
+++ mini-twitter/src/store/store.tsx
@@ -1,13 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
