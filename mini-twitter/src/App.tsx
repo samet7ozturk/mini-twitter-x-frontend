3d5dfe06ed360fe78bb9c5b129f029f353872674
samet7ozturk <sametozturk973@gmail.com>
2024-03-10 15:15:21 +0300

    Home page route added

--- mini-twitter/src/App.tsx
+++ mini-twitter/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
