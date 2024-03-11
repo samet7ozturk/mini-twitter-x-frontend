1b7b2271c3967caa903918965a360960fdc50111
samet7ozturk <sametozturk973@gmail.com>
2024-03-11 18:25:03 +0300

    Navbar complete

--- mini-twitter/src/pages/HomePage.tsx
+++ mini-twitter/src/pages/HomePage.tsx
@@ -34,58 +34,62 @@ function HomePage() {
   const onSubmit = async (data: LoginFormInput) => {};
 
   return (
-    <>
-      <div>
-        <img src={svg1} />
-        <button>
-          <img src={svg2} />
-          Home
-        </button>
-        <button>
-          <img src={svg3} />
-          Explore
-        </button>
-        <button>
-          <img src={svg4} />
-          Notifications
-        </button>
-        <button>
-          <img src={svg5} />
-          Messages
-        </button>
-        <button>
-          <img src={svg6} />
-          Bookmarks
-        </button>
-        <button>
-          <img src={svg7} />
-          Lists
-        </button>
-        <button>
-          <img src={svg8} />
-          Profile
-        </button>
-        <button>
-          <img src={svg9} />
-          More
-        </button>
-        <button>Tweet</button>
+    <div className="home-page">
+      <div className="navbar">
+        <div className="navbar-sections">
+          <img src={svg1} alt="svg1" className="logo" />
+          <div className="navbar-container">
+            <button type="button" className="navbar-button">
+              <img src={svg2} alt="svg2" />
+              Home
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg3} alt="svg3" />
+              Explore
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg4} alt="svg4" />
+              Notifications
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg5} alt="svg5" />
+              Messages
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg6} alt="svg6" />
+              Bookmarks
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg7} alt="svg7" />
+              Lists
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg8} alt="svg8" />
+              Profile
+            </button>
+            <button type="button" className="navbar-button">
+              <img src={svg9} alt="svg9" />
+              More
+            </button>
+          </div>
+          <button type="button" className="login-button tweet-button">
+            Tweet
+          </button>
+        </div>
       </div>
-      <div>
+
+      <div className="home">
         <div>
           <h1>Home</h1>
-          <img src={svg10} />
+          <img src={svg10} alt="svg10" />
         </div>
         <div>
-          <img src={png1} />
-          <textarea name="text" id="" cols={30} rows={10}></textarea>
+          <img src={png1} alt="png1" />
+          <textarea name="text" id="text" cols={30} rows={10}></textarea>
         </div>
       </div>
-      <div></div>
-
-      <h1>Log in to Twitter</h1>
-      <form onSubmit={handleSubmit(onSubmit)}></form>
-    </>
+      <article></article>
+    </div>
   );
 }
 
